92585961a548868263db8a9a0d0a401364a644c6
Vera Chernushina <chernushina.vera@gmail.com>
2022-11-15 20:40:22 +0300

    improve: apollo & vue-router settings

--- src/main.js
+++ src/main.js
@@ -1,6 +1,39 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import './main.css';
 
-createApp(App).mount('#app')
+import { createApolloClient } from '@nhost/apollo'
+import { NhostClient } from '@nhost/vue'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
+import { createRouter, createWebHistory } from 'vue-router';
+
+const nhost = new NhostClient({
+  subdomain: 'izrmzqtvvesktcetcftg',
+  region: 'eu-central-1'
+})
+
+const apolloClient = createApolloClient({ nhost })
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('./views/Home.vue'),
+    meta: {
+      protected: true,
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('./views/Login.vue'),
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+createApp(App).provide(DefaultApolloClient, apolloClient).use(nhost).mount('#app')
